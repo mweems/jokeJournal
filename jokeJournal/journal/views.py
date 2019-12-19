aa9d56cea92d588e0f1d8822d228ea4db8f3c701
Matthew Weems <matthewweems@matthews-mbp.home>
2019-12-18 16:13:31 -0800

    minor update to test gitignor

--- jokeJournal/journal/views.py
+++ jokeJournal/journal/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse("<h2>Jokes go here</h2>")
+  return HttpResponse("<h1>Jokes go here</h1>")
